c4c7172e4ba320b9c4a5f197b8ded1ef229c4cc0
Said GRICH <said.grich@edu.uca.ma>
2022-07-19 13:31:05 +0100

    finale

--- src/main/java/com/ajiw/Controllers/MainController.java
+++ src/main/java/com/ajiw/Controllers/MainController.java
@@ -1,2 +1,14 @@
-package com.ajiw.Controllers;public class MainController {
+package com.ajiw.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping()
+    public String helloWorld(){
+        return "index.html";
+    }
 }
